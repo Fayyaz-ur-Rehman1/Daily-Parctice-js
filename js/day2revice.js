f03fa3861eb8c4b5758d60bdce5b6de531a70e6e
Fayyaz <fayyazurrehman289@gmail.com>
2023-02-11 19:59:37 +0530

    Methametic-Solution

---
+++ js/day2revice.js
@@ -0,0 +1,18 @@
+
+function triArea(base,height){
+    return (base * height) / 2;
+ }
+ 
+ // `Area of a Triangle`
+ //  console.log( triArea(3, 2));
+ //  console.log( triArea(7, 4) );
+ //  console.log( triArea(10, 10) );
+
+
+function howManySeconds(hour){
+    return  3600 * hour
+}
+
+  console.log(howManySeconds(2));
+  console.log(howManySeconds(10));
+  console.log(howManySeconds(24));// hour me kitne second hote h 60 fir minutes 60 tu 60*60 fir iska answer hour se multiply hoga
